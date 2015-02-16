5e370095796dd74adf90e35d50c33979d54a7873
James Vodanovich <james@jvodan.com>
2015-02-17 00:10:51 +1100

    first commit

---
+++ engines/templates/app/config/autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "_ENGINES_dbname",
+  "dbuser"        => "_ENGINES_dbuser",
+  "dbpass"        => "_ENGINES_dbpasswd",
+  "dbhost"        => "_ENGINES_dbhost",
+  "dbtableprefix" => "",
+  "adminlogin"    => "admin",
+  "adminpass"     => "admin",
+  "directory"     => "/home/app/data",
+);
